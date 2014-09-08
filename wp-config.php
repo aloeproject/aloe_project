674bacd4627c7cc52d35fa5dac89e05705cb3ca0
lkboy <lk_qmail@foxmail.com>
2014-09-08 21:01:21 +0800

    new wordpress

---
+++ wp-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WordPress基础配置文件。
+ *
+ * 本文件包含以下配置选项：MySQL设置、数据库表名前缀、密钥、
+ * WordPress语言设定以及ABSPATH。如需更多信息，请访问
+ * {@link http://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
+ * 编辑wp-config.php}Codex页面。MySQL设置具体信息请咨询您的空间提供商。
+ *
+ * 这个文件被安装程序用于自动生成wp-config.php配置文件，
+ * 您可以手动复制这个文件，并重命名为“wp-config.php”，然后填入相关信息。
+ *
+ * @package WordPress
+ */
+require "/home/www/wp-config.php";
